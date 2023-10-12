a5cc1e8a8eda436fab7a03e00066bd86f2c048ec
giusmili <giusmili67@gmail.com>
2023-10-12 16:53:14 +0200

    update php

--- include/controllers.inc.php
+++ include/controllers.inc.php
@@ -1,11 +1,20 @@
 <?php
        const title = "Cours php"; /* constante */
        $_css = "./css/style.css";
-       $_x = true;
        $_lang = ["fr","en","es"];
        $_version = phpversion();
        $_date_now = date("d/m/Y");
        $_date_object = new dateTime();
        $_user_agent = $_SERVER["HTTP_USER_AGENT"];
        $_signature_server = $_SERVER["SERVER_SIGNATURE"];
+/* class
+    class User{
+        public $_css = "./css/style.css";
+    }
+
+    $_instance = new User()
+    $_instance->_css;
+
+*/
+
    
